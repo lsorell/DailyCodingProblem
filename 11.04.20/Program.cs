030c1b52c7af0add21ad3f78297a3267538dfd37
lanesorell <lane.sorell@ontariosystems.com>
2020-11-04 19:08:02 -0800

    Finished problem 11.04.20

---
+++ 11.04.20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11._04._20
+{
+    class Program
+    {
+        // You are given n numbers as well as n probabilities that sum up to 1. 
+        // Write a function to generate one of the numbers with its corresponding probability.
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[4] {1, 2, 3, 4};
+            double[] probabilities = new double[4] {0.1, 0.5, 0.2, 0.2};
+
+            Console.WriteLine(WeightedRandomNumber(numbers, probabilities));
+        }
+
+        public static int WeightedRandomNumber(int[] numbers, double[] probabilities)
+        {
+            double rnd = new Random().NextDouble();
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                rnd -= probabilities[i];
+                if(rnd < 0)
+                {
+                    return numbers[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}

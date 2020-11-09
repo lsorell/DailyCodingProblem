12a67adc3a34c60070b78ba2d73013c8af078dec
lanesorell <lane.sorell@ontariosystems.com>
2020-11-09 09:40:38 -0800

    Finished problem 11.09.20

---
+++ 11.09.20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11._09._20
+{
+    class Program
+    {
+        // Given a string, determine whether any permutation of it is a palindrome.
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("carrace"));
+            Console.WriteLine(IsPalindrome("daily"));
+            Console.WriteLine(IsPalindrome("nnaa"));
+            Console.WriteLine(IsPalindrome("level"));
+        }
+
+        public static bool IsPalindrome(string str)
+        {
+            HashSet<char> hs = new HashSet<char>();
+            
+            foreach(char c in str)
+            {
+                if(hs.Contains(c))
+                {
+                    hs.Remove(c);
+                }
+                else
+                {
+                    hs.Add(c);
+                }
+            }
+            
+            return (str.Length % 2 == 0) ? hs.Count == 0 : hs.Count <= 1;
+        }
+    }
+}

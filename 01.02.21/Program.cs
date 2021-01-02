4ca22f3210b8e9bd0a5d2b14639b7a197d85d918
Lane Sorell <lanesorell@gmail.com>
2021-01-02 10:05:56 -0800

    Finished problem 01.02.21

---
+++ 01.02.21/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using HelperLibrary;
+
+namespace _01._02._21
+{
+    class Program
+    {
+        // Given a string and a pattern, find the starting indices of all occurrences of the pattern in the string.
+        // For example, given the string "abracadabra" and the pattern "abr", you should return [0, 7].
+        static void Main(string[] args)
+        {
+            Helpers.PrintEnumerable<int>(FindAllPatterns("aaaaaa", "aa"));
+            Helpers.PrintEnumerable<int>(FindAllPatterns("abracadabra", "abr"));
+            Helpers.PrintEnumerable<int>(FindAllPatterns("freedomfreeters", "free"));
+        }
+
+        public static List<int> FindAllPatterns(string str, string pattern)
+        {
+            List<int> matches = new List<int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.Substring(i).Length < pattern.Length)
+                {
+                    break;
+                }
+                if (str[i] == pattern[0])
+                {
+                    bool match = true;
+                    int k = i + 1;
+                    for (int j = 1; j < pattern.Length; j++)
+                    {
+                        if (str[k] != pattern[j])
+                        {
+                            match = false;
+                            break;
+                        }
+                        k++;
+                    }
+                    if (match)
+                    {
+                        matches.Add(i);
+                    }
+                }
+            }
+            return matches;
+        }
+    }
+}
